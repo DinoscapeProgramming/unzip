6f5a6993e842529927be4ff84bcc0a0eefca7af0
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2022-02-16 17:59:20 +0100

    Update unzip.js

--- unzip.js
+++ unzip.js
@@ -4,7 +4,13 @@ const unzipper = require('adm-zip');
 function unzip(file, path) {
     const zip = new unzipper(file)
     zip.getEntries().forEach(entry => {
-        const filePath = path || "./"
+        let filePath = path || "./"
+        if (!filePath.startsWith("./")) {
+            filePath = "./" + filePath
+        }
+        if (!filePath.endsWith("/")) {
+            filePath = filePath + "/"
+        }
         fs.appendFileSync(filePath + entry.entryName, entry.getData().toString('utf8'))
     })
 }
